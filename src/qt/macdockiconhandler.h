52936a2b34f723838c11e4ff9fc5b47b467a07e7
Dwight S <dwights@usinternet.com>
2014-01-08 09:25:38 -0600

    Mac OSX build

--- src/qt/macdockiconhandler.h
+++ src/qt/macdockiconhandler.h
@@ -6,7 +6,16 @@
 class QMenu;
 class QIcon;
 class QWidget;
-class objc_object;
+// 2014-01-08 DKS Couldn't compile on Mac.  Couldn't figure it out.
+// Had some beet wine and found this http://code.metager.de/source/history/bitcoin/src/qt/macdockiconhandler.h
+// Fixed below
+// class objc_object;
+
+#ifdef __OBJC__
+@class DockIconClickEventHandler;
+#else
+class DockIconClickEventHandler;
+#endif
 
 /** Macintosh-specific dock icon handler.
  */
@@ -31,7 +40,8 @@ public slots:
 private:
     MacDockIconHandler();
 
-    objc_object *m_dockIconClickEventHandler;
+    //objc_object *m_dockIconClickEventHandler;
+    DockIconClickEventHandler *m_dockIconClickEventHandler;
     QWidget *m_dummyWidget;
     QMenu *m_dockMenu;
 };
